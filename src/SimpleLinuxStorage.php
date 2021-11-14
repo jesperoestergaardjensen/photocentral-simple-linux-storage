128f664aa9aa336d82010261a0a29b123c99dc2c
Jesper Østergaard Jensen <jesper@onlinelife.dk>
2021-11-14 22:23:30 +0100

    Implement soft delete and undo soft delete

--- src/SimpleLinuxStorage.php
+++ src/SimpleLinuxStorage.php
@@ -2,6 +2,7 @@
 
 namespace PhotoCentralSimpleLinuxStorage;
 
+use LinuxFileSystemHelper\FileHelper;
 use LinuxFileSystemHelper\FolderHelper;
 use PhotoCentralSimpleLinuxStorage\Factory\LinuxFileFactory;
 use PhotoCentralSimpleLinuxStorage\Factory\PhotoFactory;
@@ -15,6 +16,7 @@ use PhotoCentralStorage\PhotoStorage;
 class SimpleLinuxStorage implements PhotoStorage
 {
     public const PHOTO_COLLECTION_ID = 1;
+    public const TRASH_FOLDER_NAME   = '.trash/';
 
     private string $photo_path;
     private string $status_file_path;
@@ -24,13 +26,15 @@ class SimpleLinuxStorage implements PhotoStorage
      */
     private ?array $linux_file_map = null;
     private ?array $photo_map = null;
+    private string $image_cache_path;
 
-    public function __construct(string $photo_path, string $status_file_path)
+    public function __construct(string $photo_path, string $status_file_path, string $image_cache_path)
     {
         $this->photo_path = $photo_path;
         $this->status_file_path = $status_file_path;
         $this->photo_collection = new PhotoCollection(self::PHOTO_COLLECTION_ID, 'Photo folder',
             "Simple Linux Storage folder ($this->photo_path)");
+        $this->image_cache_path = $image_cache_path;
     }
 
     public function searchPhotos(string $search_string): array
@@ -39,7 +43,8 @@ class SimpleLinuxStorage implements PhotoStorage
         $this->readPhotos();
 
         foreach ($this->linux_file_map as $linux_file) {
-            str_replace($search_string, $search_string, $linux_file->getFilePath() . $linux_file->getFileName(), $count);
+            str_replace($search_string, $search_string, $linux_file->getFilePath() . $linux_file->getFileName(),
+                $count);
             if ($count > 0) {
                 $search_result_list[] = $this->photo_map[$linux_file->getPhotoUuid()];
             }
@@ -57,6 +62,7 @@ class SimpleLinuxStorage implements PhotoStorage
     ): array {
 
         $this->readPhotos();
+
         return $this->photo_map;
     }
 
@@ -75,19 +81,68 @@ class SimpleLinuxStorage implements PhotoStorage
     {
         $this->readPhotos();
 
-        // TODO: Implement getPhotos() method.
-        return [];
+        $photo_list = [];
+
+        foreach ($photo_uuid_list as $photo_uuid) {
+            $photo_list[] = $this->getPhoto($photo_uuid);
+        }
+
+        return $photo_list;
     }
 
     public function softDeletePhoto(string $photo_uuid): bool
     {
-        // TODO: Implement softDeletePhoto() method.
+        $this->readPhotos();
+
+        if (isset($this->photo_map[$photo_uuid]) === false) {
+            throw new PhotoCentralStorageException("No photo could be found with the supplied uuid $photo_uuid");
+        }
+
+        // Create trash folder
+        FolderHelper::createFolder($this->photo_path . self::TRASH_FOLDER_NAME);
+
+        // Get file to be deleted
+        $linux_file_to_softe_delete = $this->linux_file_map[$photo_uuid];
+
+        // Build trash destination folder name and create it
+        $trash_folder_destination = $this->photo_path . self::TRASH_FOLDER_NAME . $linux_file_to_softe_delete->getFilePath();
+        FolderHelper::createFolder($trash_folder_destination);
+
+        // Softdelete file by moving it into trash folder
+        FileHelper::moveFile($linux_file_to_softe_delete->getFullFileNameAndPath($this->photo_path),
+            $trash_folder_destination . $linux_file_to_softe_delete->getFileName());
+
+        // Remove folder after moving file to trash if folder is empty
+        $folder_to_remove = $this->photo_path . $linux_file_to_softe_delete->getFilePath();
+        $this->removeUnusedFoldersRecursively($folder_to_remove);
+
         return true;
     }
 
     public function undoSoftDeletePhoto(string $photo_uuid): bool
     {
-        // TODO: Implement undoSoftDeletePhoto() method.
+        // Build a list of deleted files
+        $deleted_linux_file_map = $this->buildDeletedLinuxFilesMap();
+
+        // Get the file that should be un-deleted
+        $linux_file_to_undelete = $deleted_linux_file_map[$photo_uuid];
+
+        // build source path and file name
+        $source_file_name_and_path = $linux_file_to_undelete->getFullFileNameAndPath($this->photo_path . self::TRASH_FOLDER_NAME);
+
+        // re-build orgiginal path and file name
+        $original_destination_file_name_and_folder = $linux_file_to_undelete->getFullFileNameAndPath($this->photo_path);
+
+        // re-create folder that the deleted file was in
+        FolderHelper::createFolder($this->photo_path . $linux_file_to_undelete->getFilePath());
+
+        // Undo soft delete by moving file back to original folder
+        FileHelper::moveFile($source_file_name_and_path, $original_destination_file_name_and_folder);
+
+        // Remove folder inside trash (including trash) if empty
+        $folder_to_remove = $this->photo_path . self::TRASH_FOLDER_NAME . $linux_file_to_undelete->getFilePath();
+        $this->removeUnusedFoldersRecursively($folder_to_remove);
+
         return true;
     }
 
@@ -96,11 +151,37 @@ class SimpleLinuxStorage implements PhotoStorage
         return [$this->photo_collection];
     }
 
+    private function removeUnusedFoldersRecursively(string $path): void {
+        if (FolderHelper::isFolderEmpty($path)) {
+            rmdir($path);
+            $this->removeUnusedFoldersRecursively(dirname($path));
+        }
+    }
+
+    /**
+     * @return LinuxFile[]
+     */
+    private function buildDeletedLinuxFilesMap(): array
+    {
+        $deleted_linux_file_map = [];
+
+        $deleted_file_list = FolderHelper::listFilesRecursiveFromFolder($this->photo_path . self::TRASH_FOLDER_NAME,
+            '.jpg');
+
+        foreach ($deleted_file_list as $deleted_file) {
+            $deleted_linux_file = LinuxFileFactory::createLinuxFile($deleted_file,
+                $this->photo_path . self::TRASH_FOLDER_NAME);
+            $deleted_linux_file_map[$deleted_linux_file->getPhotoUuid()] = $deleted_linux_file;
+        }
+
+        return $deleted_linux_file_map;
+    }
+
     private function readPhotos()
     {
         if ($this->linux_file_map === null && $this->photo_map === null) {
             // TODO: Simple Linux Storage should have a upper limit
-            $jpg_file_list = FolderHelper::listFilesRecursiveFromFolder($this->photo_path, '.jpg', ['.trash']);
+            $jpg_file_list = FolderHelper::listFilesRecursiveFromFolder($this->photo_path, '.jpg', [trim(self::TRASH_FOLDER_NAME, '/')]);
 
             foreach ($jpg_file_list as $jpg_file) {
                 $new_linux_file = LinuxFileFactory::createLinuxFile($jpg_file, $this->photo_path);
--- tests/unit/SimpleLinuxStorageTest.php
+++ tests/unit/SimpleLinuxStorageTest.php
@@ -4,21 +4,27 @@ namespace PhotoCentralSimpleLinuxStorage\Tests\unit;
 
 use PhotoCentralSimpleLinuxStorage\Factory\PhotoUuidFactory;
 use PhotoCentralSimpleLinuxStorage\SimpleLinuxStorage;
+use PhotoCentralStorage\Exception\PhotoCentralStorageException;
 use PHPUnit\Framework\TestCase;
 
 class SimpleLinuxStorageTest extends TestCase
 {
     private SimpleLinuxStorage $simple_linux_storage;
 
-    private const TEST_PHOTO_FILE_NAME = 'coffee-break.jpg';
+    private const DELETED_PHOTO_UUID_1 = 'e9be5e89fc397c680580599e1f3ef21e';
+    private const DELETED_PHOTO_UUID_2 = '8fae4586a47a2356df0ea12c997e047e';
 
-    private function getPhotosTestFolder(): string {
+    private const TEST_PHOTO_FILE_NAME_1 = 'coffee-break.jpg';
+    private const TEST_PHOTO_FILE_NAME_2 = 'sport/mtb/mountain-bike-g30008f9d7_1280.jpg';
+
+    private function getPhotosTestFolder(): string
+    {
         return dirname(__DIR__) . "/data/photos/";
     }
 
     public function setUp(): void
     {
-        $this->simple_linux_storage = new SimpleLinuxStorage($this->getPhotosTestFolder(),'b');
+        $this->simple_linux_storage = new SimpleLinuxStorage($this->getPhotosTestFolder(), 'b', 'c');
     }
 
     public function testListPhotoCollections()
@@ -27,20 +33,43 @@ class SimpleLinuxStorageTest extends TestCase
 
         $this->assertCount(1, $photo_collection_list, 'One item in the list is expected');
         $this->assertEquals('1', $photo_collection_list[0]->getId(), 'id is expected to be 1');
-        $this->assertEquals('Photo folder', $photo_collection_list[0]->getName(), 'name is expected to be "Photo folder"');
+        $this->assertEquals('Photo folder', $photo_collection_list[0]->getName(),
+            'name is expected to be "Photo folder"');
         $photo_folder = $this->getPhotosTestFolder();
         $this->assertEquals("Simple Linux Storage folder ($photo_folder)", $photo_collection_list[0]->getDescription());
     }
 
     public function testGetPhoto()
     {
-        $test_photo_file = self::getPhotosTestFolder() . self::TEST_PHOTO_FILE_NAME;
+        $test_photo_file = self::getPhotosTestFolder() . self::TEST_PHOTO_FILE_NAME_1;
 
         $photo_uuid = PhotoUuidFactory::generatePhotoUuid($test_photo_file);
 
         $photo = $this->simple_linux_storage->getPhoto($photo_uuid);
 
         $this->assertEquals('ASUS', $photo->getCameraBrand());
+        $this->assertEquals(386, $photo->getHeight());
+        $this->assertEquals(686, $photo->getWidth());
+
+        $this->expectException(PhotoCentralStorageException::class);
+        $this->simple_linux_storage->getPhoto('non-existing-uuid');
+    }
+
+    public function testGetPhotos()
+    {
+        $test_photo_file_1 = self::getPhotosTestFolder() . self::TEST_PHOTO_FILE_NAME_1;
+        $test_photo_file_2 = self::getPhotosTestFolder() . self::TEST_PHOTO_FILE_NAME_2;
+
+        $photo_uuid_1 = PhotoUuidFactory::generatePhotoUuid($test_photo_file_1);
+        $photo_uuid_2 = PhotoUuidFactory::generatePhotoUuid($test_photo_file_2);
+
+        $photo_list = $this->simple_linux_storage->getPhotos([$photo_uuid_1, $photo_uuid_2]);
+
+        $this->assertCount(2, $photo_list);
+
+        $this->expectException(PhotoCentralStorageException::class);
+        $this->simple_linux_storage->getPhotos([$photo_uuid_1, 'non-existing-uuid']);
+
     }
 
     public function testListPhotos()
@@ -57,6 +86,21 @@ class SimpleLinuxStorageTest extends TestCase
 
         $search_result_list = $this->simple_linux_storage->searchPhotos('coffee');
         $this->assertCount(1, $search_result_list, 'one image should be found');
+    }
+
+    public function testSoftDelete()
+    {
+        $this->assertTrue($this->simple_linux_storage->softDeletePhoto(self::DELETED_PHOTO_UUID_1));
+        $this->assertTrue($this->simple_linux_storage->softDeletePhoto(self::DELETED_PHOTO_UUID_2));
+    }
+
+    /**
+     * @depends testSoftDelete
+     */
+    public function testUndoSoftDeletePhoto()
+    {
+        $this->assertTrue($this->simple_linux_storage->undoSoftDeletePhoto(self::DELETED_PHOTO_UUID_1));
+        $this->assertTrue($this->simple_linux_storage->undoSoftDeletePhoto(self::DELETED_PHOTO_UUID_2));
     }
 }
 
